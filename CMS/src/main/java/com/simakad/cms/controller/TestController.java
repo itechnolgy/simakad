8f3aa494644ebcb823c33f1f02ac11f6dd374c0f
kovan.chan@samsung.com <kovan.chan@samsung.com>
2016-09-13 18:53:48 +0700

    1. Setup thymeleaf

--- CMS/src/main/java/com/simakad/cms/controller/TestController.java
+++ CMS/src/main/java/com/simakad/cms/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.simakad.cms.controller;
 
+import com.simakad.dao.repo.Student;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,15 @@ Note : comment rest controller annotation
 and uncomment controller annotate after implement thymeleaf !!
 This is just for example !
  */
-@RestController
-//@Controller
+//@RestController
+@Controller
 public class TestController {
     @RequestMapping(value = "/test")
-    public String cetak() {
-        return "Kovan's here !!";
+    public String cetak(Model model) {
+        Student student = new Student();
+        student.setName("kovan");
+        student.setAddress("cengkareng");
+        model.addAttribute(student);
+        return "test/login";
     }
 }
